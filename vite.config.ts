851804ccd791f3a2f503a559e82716a26c7609e8
JS0Nai <working@monarkh.net>
2024-11-10 14:06:20 +0400

    WORKING - modifications to vite and prepare-pyodide files

--- vite.config.ts
+++ vite.config.ts
@@ -26,5 +26,14 @@ export default defineConfig({
 	},
 	worker: {
 		format: 'es'
-	}
+	},
+	server: {
+		proxy: {
+			'/api': {
+				target: 'http://127.0.0.1:4568',
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api/, '/api')
+			},
+		},
+	},
 });
